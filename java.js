4ef686c7a2e47b84323c3bf59fd2e8f1fdbe60c2
arunthacharuthodi <67404902+arunthacharuthodi@users.noreply.github.com>
2020-10-23 18:02:26 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,111 @@
+//-------this website made by brain-tech handed with th.inc -----the whole website copyrighted for the devoleper-------------------
+//-------------------------------------------COPYRIGHTS 2020 arunthacharuthodi-----------------------------------------------------
+//-------------------------------------------email:arunthacharuthodi@gmail.com-----------------------------------------------------
+
+const first  = document.getElementById("btn-1");
+const second = document.getElementById("btn-2");
+const third  = document.getElementById("btn-3");
+const fourth = document.getElementById("btn-4");
+const fifth  = document.getElementById("btn-5");
+
+//btn-1
+function fn1() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page  \n #troll #funny #comedy #meme #lol #fortnite #memes #instagram #youtube #love #gamer #twitter #twitch #dank #humor #fortnitebattleroyale #instagood #trolling #fun #fortnitememes #gaming #fortnitecommunity #funnymemes #fortnitexbox #ninja #savage #laugh #trump #xboxone";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+
+//btn-2
+function fn2() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n . . . #malayalamtrolls #trolls #epic #mallu #mallutroll #trollmalayalam #trollen #met_trollers #malappuram #alappuzha #wayanad #kasarkod #kerala #fresh #fresh_troll #followers #followher #follower #followhim #followbackteam #followall #followbackalways #follows #following #followgram #followshoutoutlikecomment #follownow #followus ";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+
+//btn-3
+function fn3() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #trollhunters #trollmalayalam #bakchodi #instagood #pubg #bollywood #indian #memer #followforfollowback #trollguru #likeforlikes #malayalamtrolls #mysore #trendingmemes #chutiyapa #thalapathy #malayali #bengaluru #art #indianmemes #mallutrolls #mohanlal #kannadatrolls #youtube #laugh #vijay #kgf #friends #viral #humour";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+
+//btn-4
+function fn4() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #troll #memes #trolls #meme #funny #tiktok #kerala #instagram #funnymemes #fun #trending #love #lol #memesdaily #comedy #official #follow #like #tamil #kannada #sarcasm #karnataka #india #jokes #tamilmemes #trolling #dankmemes #malayalam #corona #bhfyp";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+[]
+//btn-5
+function fn5() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #mom #trollmalayalam #sister #cod#pubg #bollywood #indian #dream #hotmallu #instamodels#likeforlikes #malayalamtrolls #model #crazzy #friends #thalapathy #malayali #bike #art #indianmemes #mallutrolls #mohanlal #black#youtube #laugh #vijay #kgf #friends #indiannavy";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+//btn-6
+function fn6() {
+
+var textelemen = document.getElementById("mytext").value;
+var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n. . . #malayalamtrolls #trolls #epic #mallu #mallutroll #trollmalayalam #trollen #met_trollers #malappuram #alappuzha #wayanad #kasarkod #kerala #fresh #lovers #followers #mine#follower #srk #followbackteam #indianarmy #indiannavy #follows #girl #football #single #follownow #india";
+var a = textelemen+"\n"+"\n"+one;
+document.getElementById("view-box").value = a;
+};
+
+//MS1
+function MS1() {
+
+    var textelemen = document.getElementById("mytext").value;
+    var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #movies #movie #film #cinema #films #hollywood #love #actor #s #art #actress #cinematography #netflix #music #moviescenes #bollywood #instagood #filmmaking #horror #instagram #movienight #comedy #cine #cinephile #photography #drama #tv #director #bhfyp #bhfyp";
+    var a = textelemen+"\n"+"\n"+one;
+    document.getElementById("view-box").value = a;
+    };
+
+//MS2
+function MS2() {
+
+    var textelemen = document.getElementById("mytext").value;
+    var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #series #horrormovies #actors #moviereview #follow #like #bluray #video #moviestar #instamovies #moviequotes #peliculas #dvd #entertainment #memes #photooftheday #marvel #movietime #fashion #movielover #filmes #action #moviebuff #disney #quotes #filmphotography #thriller #acting #artist #podcast";
+    var a = textelemen+"\n"+"\n"+one;
+    document.getElementById("view-box").value = a;
+    };
+
+//MS3
+function MS3() {
+
+    var textelemen = document.getElementById("mytext").value;
+    var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #movie #movies #movienight #horrormovies #movietime #moviestar #instamovies #horrormovie #tamilmovie #aftermovie #goodmovie #moviescenes #moviequotes #bollywoodmovies #tamilmovies #itmovie #instamovie #moviedate #moviereview #netflixmovies #malayalammovie #movielover #moviebuff #bollywoodmovie #movieday #movieposter #telugumovie #moviemaking #jokermovie #disneymovie";
+    var a = textelemen+"\n"+"\n"+one;
+    document.getElementById("view-box").value = a;
+    };
+//copy button
+function MS4() {
+
+    var textelemen = document.getElementById("mytext").value;
+    var one = "Mallu epic trollers troll page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n Follow us @m.e.t_troll_page \n #kannadamovies #nollywoodmovies #bestmovie #scarymovie #moviecollection #disneymovies #imovie #favoritemovie #shortmovie #classicmovies #marvelmovies #telugumovies #80smovies #moviecollector #movieaddict #afterwecollidedmovie #movieworld #oldmovies #scarymovies #moviescene #movietheater #legomovie #movielovers #greatmovie #movietime🎬 #moviestarplanet";
+    var a = textelemen+"\n"+"\n"+one;
+    document.getElementById("view-box").value = a;
+    };
+
+function copyfn() {
+var copycont = document.getElementById("view-box");
+copycont.select();
+copycont.setSelectionRange(0, 99999)
+document.execCommand("copy");
+alert(copycont.value);
+
+};
